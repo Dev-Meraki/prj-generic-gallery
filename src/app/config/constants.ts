06c9fe98a0d5a7ad90479a9d0fe8a3eecce4b353
HS <himanshusaraswat@outlook.com>
2022-03-24 19:09:54 +0530

    Refactor constants

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -5,3 +5,10 @@ export interface GALLERY {
   items: Array<string>;
   nextPageToken: string | undefined;
 }
+
+export const ACTION_ERROR = {
+  ERROR:'ERROR',
+  SUCCESS:'SUCCESS',
+  MAX_LIMIT: `You can borrow only $LIMIT books`,
+  COPIES_ALLOWED: `You can borrow only $DISTINCT copy of`,
+};
