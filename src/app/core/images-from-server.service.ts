eef30652d10129cde39618b9527c25dfe954d015
Himanshu Saraswat <himanshusaraswat@outlook.com>
2025-04-09 23:07:28 +0530

    fix: have a variation of httpResource

--- src/app/core/images-from-server.service.ts
+++ src/app/core/images-from-server.service.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
+import { HttpClient, httpResource } from '@angular/common/http';
+import { Injectable, Signal, computed, inject, signal } from '@angular/core';
 import { MarvelApiResponse, Modes, Character } from '../shared/interfaces';
 import { APP_MODES, ACTION, INIT_GALLERY_STATE } from '../shared/contants';
 
@@ -12,43 +12,78 @@ import { LoaderService } from './loader.service';
 export class ImagesFromServerService {
   private http = inject(HttpClient);
   private loader = inject(LoaderService);
+  private gameIsInPlay = signal<boolean>(false);
+  gameIsInPlaySignal = this.gameIsInPlay.asReadonly();
 
   private images = signal<MarvelApiResponse>(INIT_GALLERY_STATE);
   imagesSignal = this.images.asReadonly();
 
-  private getQueryParams(
-    action: string,
-    mode: Modes | string,
-    currentPageState: number
-  ) {
-    if (action === ACTION.init && mode === APP_MODES.gallery) {
-      return `limit=10&offset=0`;
-    } else if (mode === APP_MODES.play) {
-      return `limit=1&offset=${currentPageState + 1}`;
-    } else {
-      return `limit=10&offset=${(currentPageState + 1) * 10}`;
-    }
+  private action = signal(ACTION.init);
+  private mode = signal(APP_MODES.gallery);
+  private currentPage = signal(0);
+
+  setParams(action: string, mode: Modes | string) {
+    this.currentPage.update((currentPageValue) => currentPageValue + 1);
+    this.action.update(() => action);
+    this.mode.update(() => mode);
   }
 
-  public getImages(action: string, mode: Modes | string = APP_MODES.gallery) {
-    this.loader.setLoaderTo(true);
-    const currentPageState = this.getGalleryState().page;
+  public getImages() {
+    this.getImagesFromServer().subscribe((response: MarvelApiResponse) => {
+      this.action() === ACTION.paginate && this.mode() === APP_MODES.gallery
+        ? this.generateStatePostPagination(response)
+        : this.images.set({
+            ...response,
+          });
+      this.loader.setLoaderTo(false);
+    });
+  }
 
-    const queryParams = this.getQueryParams(action, mode, currentPageState);
-    this.getImagesFromServer(queryParams).subscribe(
-      (response: MarvelApiResponse) => {
-        action === ACTION.paginate && mode === APP_MODES.gallery
-          ? this.generateStatePostPagination(response, currentPageState)
-          : this.images.set({
-              ...response,
-              page: currentPageState + 1,
-            });
-        this.loader.setLoaderTo(false);
-      }
+  private queryParamStr: Signal<string> = computed(() => {
+    if (this.action() === ACTION.init && this.mode() === APP_MODES.gallery) {
+      return `limit=10&offset=${this.currentPage()}`;
+    } else if (this.mode() === APP_MODES.play) {
+      return `limit=1&offset=${this.currentPage() + 1}`;
+    } else {
+      return `limit=10&offset=${this.currentPage() * 10}`;
+    }
+  });
+
+  private getImagesFromServer() {
+    return this.http.get<MarvelApiResponse>(
+      `${environment.API_URL}public/characters?${this.queryParamStr()}&apikey=${
+        environment.API_KEY
+      }`
     );
   }
 
-  public getGalleryState(): MarvelApiResponse {
+  // Uncomment for resource demo
+  // getImagesResource = httpResource<MarvelApiResponse>(
+  //   () =>
+  //     `${environment.API_URL}public/characters?${this.queryParamStr()}&apikey=${
+  //       environment.API_KEY
+  //     }`,
+  //   {
+  //     defaultValue: INIT_GALLERY_STATE,
+  //     // parse: (rawResponse: any) => {
+  //     //   // this.loader.setLoaderTo(false);
+  //     //   this.images.set({
+  //     //     ...rawResponse,
+  //     //   });
+  //     //   return {
+  //     //     ...rawResponse,
+  //     //   };
+  //     // },
+  //   }
+  // );
+
+  resetPlayground(action: string, mode: string) {
+    this.currentPage.update(() => 0);
+    this.action.update(() => action);
+    this.mode.update(() => mode);
+  }
+
+  private getGalleryState(): any {
     return this.imagesSignal();
   }
 
@@ -56,31 +91,9 @@ export class ImagesFromServerService {
     this.images.set({ ...INIT_GALLERY_STATE });
   }
 
-  // TODO: Error Handling of http request
-  private getImagesFromServer(queryParamStr: string) {
-    // return this.http.get<any>(`${this.BASE_URL}/headshots?${queryParamStr}`);
-    return this.http.get<MarvelApiResponse>(
-      `${environment.API_URL}public/characters?${queryParamStr}&apikey=${environment.API_KEY}`
-    );
-  }
-
-  getResult(name: string | undefined): boolean {
-    // return this.http.get<any>(
-    //   `${this.BASE_URL}/headshots/gameResult?name=${name}&id=${id}`
-    // );
-    if (!name) return false;
-    const characterName =
-      this.getGalleryState()?.data?.results[0].name.toLowerCase();
-    return name === characterName;
-  }
-
-  private generateStatePostPagination(
-    response: MarvelApiResponse,
-    currentPageState: number
-  ) {
+  private generateStatePostPagination(response: MarvelApiResponse) {
     this.images.set({
       ...response,
-      page: currentPageState + 1, // Here, page is an additional field added.
       data: {
         ...response.data,
         results: [
@@ -89,5 +102,17 @@ export class ImagesFromServerService {
         ],
       },
     });
+  }
+
+  // Play Mode
+  setGameIsInPlay(inPlay: boolean) {
+    this.gameIsInPlay.update(() => inPlay);
+  }
+
+  getResult(name: string | undefined): boolean {
+    if (!name) return false;
+    const characterName =
+      this.getGalleryState()?.data?.results[0].name.toLowerCase();
+    return name === characterName;
   }
 }
